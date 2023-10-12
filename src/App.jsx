7c5f99def63e56195d2f56f1f764fe4978d70cc5
Eduard Tesvintsev <tesvintsev@gmail.com>
2023-10-12 20:37:25 +0300

    Journal Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import CardButton from './components/CardButton/CardButton';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
+import JournalForm from './components/JournalForm/JournalForm';
 import JournalItem from './components/JournalItem/JournalItem';
 import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/Body/Body';
@@ -10,7 +11,7 @@ import LeftPanel from './layouts/LeftPanel/LeftPanel';
 function App() {
 	const data = [
 		{
-			title: 'Подготовка к добавлению заметок',
+			title: 'Подготовка к обновлению курсов',
 			text: 'Горные походы открывают удивительные природные ландшафт',
 			date: new Date()
 		},
@@ -44,7 +45,7 @@ function App() {
 				</JournalList>
 			</LeftPanel>
 			<Body>
-				Body
+				<JournalForm/>
 			</Body>
 		</div>
 	);
